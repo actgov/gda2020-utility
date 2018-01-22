7c5225f9a068355ea5d43e7880d382583d40f8b6
Aaron O'Hehir <31329166+AOHehir@users.noreply.github.com>
2018-01-22 14:20:49 +1100

    Update gda2020_utility.py
    - The NADJ names are now retained in the first column (as opposed to the previous version where they were overwritten with jurisdictional station names.
    - Jurisdiction station names have been added to the final column. 
    - The Description column has been removed.
    - Additional information has been added to the metadata
    - The time in the metadata has been changed from GTM to AEST.
    - Variables with 'ACT' in their names have been changed to 'jurisdiction'
    - Jurisdiction has been added as an input parameter. The jurisdiction name is printed in the metadata.
    - End of report has been added.

--- gda2020_utility.py
+++ gda2020_utility.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from time import gmtime, strftime
+from datetime import datetime
+from dateutil import tz
 import argparse
 import sys
 
@@ -41,11 +43,14 @@ def clean_input_list(mark_list):
 def main():
     """Process entrypoint"""
     parser = argparse.ArgumentParser(description='Create jurisdiction subset from national marks list')
-    parser.add_argument('--jurisdiction_marks_in', help='input jurisdiction marks file', type=str, required=True)
+    parser.add_argument('--jurisdiction_name', help='Name of Jurisdiction i.e. ACT.', type=str, required=True)
+    parser.add_argument('--jurisdiction_marks_in', help='Input jurisdiction marks file', type=str, required=True)
     parser.add_argument('--national_marks_in', help='Input national adjustment file', type=str, required=True)
     parser.add_argument('--subset_out', help='path, file name and extension to write subset results to.', type=str, required=True)
+    
     args = parser.parse_args()
 
+    jurisdiction_name = args.jurisdiction_name
     jurisdiction_input_file = args.jurisdiction_marks_in
     national_input_file = args.national_marks_in
     jurisdiction_output_file = args.subset_out
@@ -64,32 +69,51 @@ def main():
     f = open(national_input_file, 'r')
 
     parsed_national_survey_marks = sheet1.icol(0).real
-    parsed_act_survey_marks = sheet1.icol(1).real
+    parsed_jurisdiction_survey_marks = sheet1.icol(1).real
 
     national_survey_marks = clean_input_list(parsed_national_survey_marks)
-    act_survey_marks = clean_input_list(parsed_act_survey_marks)
+    jurisdiction_survey_marks = clean_input_list(parsed_jurisdiction_survey_marks)
     national_input_file_as_list = f.readlines()
 
     # Transfer metadata from national adjustment to output
-    act_marks_to_output = national_input_file_as_list[0:14]
-    act_marks_to_output.append('Input NADJ file: ' + national_input_file + '\n')
-    act_marks_to_output.append('Input Jurisdiction mark list: ' + jurisdiction_input_file + '\n')
-    act_marks_to_output.append('Date Processed: ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '\n')
-    act_marks_to_output.append('\n')
-    act_marks_to_output += national_input_file_as_list[15:20]
+    jurisdiction_marks_to_output = national_input_file_as_list[0:14]
+    jurisdiction_marks_to_output.append('EXTRACTION OF ' + jurisdiction_name + ' SURVEY CONTROL MARKS FROM NATIONAL GDA2020 ADJUSTMENT:\n')
+    jurisdiction_marks_to_output.append('Input NADJ file: ' + national_input_file + '\n')
+    jurisdiction_marks_to_output.append('Input '+ jurisdiction_name +' mark list: ' + jurisdiction_input_file + '\n')
+
+    # Get time for metadata
+    # Auto-detect zones:
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+    utc = datetime.utcnow()
+    # Tell the datetime object that it's in UTC time zone since 
+    # datetime objects are 'naive' by default
+    utc = utc.replace(tzinfo=from_zone)
+    # Convert time zone
+    aest = utc.astimezone(to_zone)
+    date_str = "%d-%d-%d %d:%d:%d" % (aest.year, aest.month, aest.day, aest.hour, aest.minute, aest.second)
+
+    jurisdiction_marks_to_output.append('Date-time Processed: ' + date_str + ' AEST\n')
+    jurisdiction_marks_to_output.append('NOTES:\n')
+    jurisdiction_marks_to_output.append('(1) h(Ellipse) = Height above the GRS80 ellipsoid.\n')
+    jurisdiction_marks_to_output.append('(2) H(Ortho)   = Orthometric height (derived AHD)\n')
+    jurisdiction_marks_to_output.append('\n')
+    national_input_file_as_list[18] = national_input_file_as_list[18].replace('Description', jurisdiction_name+' Station Name')
+    jurisdiction_marks_to_output += national_input_file_as_list[15:20]
 
     for row_containing_mark in national_input_file_as_list[20:]:
         mark_name_nadj = row_containing_mark[:20].rstrip()
         for mark_name in national_survey_marks:
-            if mark_name == mark_name_nadj and act_survey_marks[national_survey_marks.index(mark_name)] != 'adopt_national_mark_name':
-                adopted_mark_name = act_survey_marks[national_survey_marks.index(mark_name)] + (' ' * 50)
-                adopted_mark_name = adopted_mark_name[:20]
-                act_marks_to_output.append(adopted_mark_name + row_containing_mark[20:])
-            elif mark_name == mark_name_nadj and act_survey_marks[national_survey_marks.index(mark_name)] == 'adopt_national_mark_name':
-                act_marks_to_output.append(row_containing_mark)
+            if mark_name == mark_name_nadj and jurisdiction_survey_marks[national_survey_marks.index(mark_name)] != 'adopt_national_mark_name':
+                adopted_mark_name = jurisdiction_survey_marks[national_survey_marks.index(mark_name)] + (' ' * 100)
+                adopted_mark_name = adopted_mark_name[:40]
+                jurisdiction_marks_to_output.append(row_containing_mark[:193] + adopted_mark_name)
+            elif mark_name == mark_name_nadj and jurisdiction_survey_marks[national_survey_marks.index(mark_name)] == 'adopt_national_mark_name':
+                jurisdiction_marks_to_output.append(row_containing_mark[:193])
+    jurisdiction_marks_to_output.append('------------------------    END    OF    REPORT   ------------------------\n')
     f = open(jurisdiction_output_file, 'r+')
     f.seek(0)
-    f.writelines(act_marks_to_output)
+    f.writelines(jurisdiction_marks_to_output)
     f.truncate()
     f.close()
 
